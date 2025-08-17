a63dd4efd84e18a782a2b220b04a657b11e71703
Voronina Ekaterina <voroninaekaterina401@gmail.com>
2025-08-17 11:49:51 +0300

    implementation: add migrations, jpa users, spring security, spring data

---
+++ src/main/java/com/example/models/User.java
@@ -0,0 +1,18 @@
+package com.example.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+}
